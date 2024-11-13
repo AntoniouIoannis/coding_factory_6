4687f8d5858c8c9d2194851b714315772c38d476
i-antoniou || John Antoniou <antoniouioannis10@gmail.com>
2024-11-13 17:43:22 +0200

    initial code

---
+++ chapter01/hello_world.py
@@ -0,0 +1,13 @@
+print()
+print("Start of program.........................")
+print()
+print("hello world.")
+
+print()
+print("*****************************************")
+print()
+
+name = "Giannis Antoniou"
+print(f"Hello World from ", name,  "!!")
+
+print("..........................End of program.")
